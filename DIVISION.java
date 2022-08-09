0fd2a20897af203775c4c2e08df89d7bfa7cfe20
AKASHPAL123456 <110466299+AKASHPAL123456@users.noreply.github.com>
2022-08-09 18:06:47 +0530

    Add files via upload

---
+++ DIVISION.java
@@ -0,0 +1,12 @@
+class DIVISION
+{
+public static void main(String []args)
+{
+System.out.println("The Two Number is :");
+int M = 200;
+int N = 20;
+System.out.println(M + " " + N);
+int division = M/N;
+System.out.println(" The division is :" + division);
+}
+}
