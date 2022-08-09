ab0f638787ae4b7f44c729afd0e14d78dc9d0a76
AKASHPAL123456 <110466299+AKASHPAL123456@users.noreply.github.com>
2022-08-09 17:52:47 +0530

    Add files via upload

---
+++ SUBSTRACTION.java
@@ -0,0 +1,12 @@
+class SUBSTRACTION
+{
+public static void main(String []args)
+{
+System.out.println("The Two Number is :");
+int a = 25;
+int b = 10;
+System.out.println(a + " " + b);
+int substraction = a-b;
+System.out.println(" The substraction is :" + substraction);
+}
+}
