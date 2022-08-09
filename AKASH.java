1d6926ac2e83e6c57709249a9f0d8f3365a2c1c2
AKASHPAL123456 <110466299+AKASHPAL123456@users.noreply.github.com>
2022-08-09 16:38:49 +0530

    Add files via upload

---
+++ AKASH.java
@@ -0,0 +1,12 @@
+class AKASH
+{
+   static int c = 30;
+   int a = 50;
+public static void main(String[]args)
+{
+  AKASH pal = new AKASH();
+  int b = 60;
+  System.out.println(AKASH.c);
+System.out.println(b);
+System.out.println(pal.a);
+}}
