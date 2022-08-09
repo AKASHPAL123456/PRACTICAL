1112e96ab2e3d61e765e1f11dcfe96690c9143dc
AKASHPAL123456 <110466299+AKASHPAL123456@users.noreply.github.com>
2022-08-09 17:59:25 +0530

    Add files via upload

---
+++ MULTIPLICATION.java
@@ -0,0 +1,12 @@
+class MULTIPLICATION
+{
+public static void main(String []args)
+{
+System.out.println("The Two Number is :");
+int d = 20;
+int c = 15;
+System.out.println(d + " " + c);
+int multiply = c*d;
+System.out.println(" The substraction is :" + multiply);
+}
+}
