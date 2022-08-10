a17ad953f6f80af0715aace2735446ab949283ae
AKASHPAL123456 <110466299+AKASHPAL123456@users.noreply.github.com>
2022-08-10 15:36:23 +0530

    Add files via upload

---
+++ AREAOFSQUARE.java
@@ -0,0 +1,11 @@
+class AREAOFSQUARE
+{
+public static void main(String []args)
+{
+System.out.println("Side of Square :");
+int a = 5;
+System.out.println(a);
+int area = a*a;
+System.out.println(" The area of square is :" + area);
+}
+}
