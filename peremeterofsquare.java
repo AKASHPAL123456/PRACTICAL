9e485303d643310135d412e9bcc5d001d6aef805
AKASHPAL123456 <110466299+AKASHPAL123456@users.noreply.github.com>
2022-08-10 15:44:30 +0530

    Add files via upload

---
+++ peremeterofsquare.java
@@ -0,0 +1,11 @@
+class peremeterofsquare
+{
+public static void main(String []args)
+{
+System.out.println("Side of Square :");
+int a = 5;
+System.out.println(a);
+int peremeter = 4*a;
+System.out.println(" The peremeter of square is :" + peremeter);
+}
+}
