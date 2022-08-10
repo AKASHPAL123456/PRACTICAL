e6139a0762995db5502c7a9caf9887a2162754e1
AKASHPAL123456 <110466299+AKASHPAL123456@users.noreply.github.com>
2022-08-10 16:11:11 +0530

    Add files via upload

---
+++ Rectangleperemeter.java
@@ -0,0 +1,14 @@
+class Rectangleperemeter
+{
+public static void main(String []args)
+{
+System.out.println("The Length of Rectangle:");
+int m = 15;
+System.out.println(m);
+System.out.println("The Width of Rectangle:");
+int n = 10;
+System.out.println(n);
+int Peremeter = 2*(m+n);
+System.out.println("The Peremeter of Rectangle is :" + Peremeter);
+}
+}
