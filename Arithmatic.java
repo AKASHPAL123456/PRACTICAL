533053a3667531d6308193a727c71dc503ec4cca
AKASHPAL123456 <110466299+AKASHPAL123456@users.noreply.github.com>
2022-08-17 17:31:17 +0530

    Add files via upload

---
+++ Arithmatic.java
@@ -0,0 +1,24 @@
+package Operator;
+
+public class Arithmatic {
+
+	public static void main(String[] args) {
+	   int a = 10;
+	   int b = 20;
+	   System.out.println(a+b);
+	   System.out.println(a-b);
+	   System.out.println(a*b);
+	   System.out.println(a/b);
+	   System.out.println(a%b);
+	   
+	   System.out.println(a<b&&b<a);
+	   System.out.println(a>b&&b<a);
+	   System.out.println(a<b||b<a);
+	   System.out.println(a>b||b<a);
+	   
+		System.out.println(a++ + ++a);
+		System.out.println(b-- + --b);
+
+	}
+
+}
